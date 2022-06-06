24f037e1b0520654691058d4f2e1d8a9b06d9ea7
Max <Max>
2022-06-06 18:37:39 +0200

    Add Receiver/Sender

--- src/com/nuvoled/Main.java
+++ src/com/nuvoled/Main.java
@@ -4,18 +4,24 @@ import java.util.Objects;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static String addr;
 
+    public static void main(String[] args) {
         for(int i = 0; i < args.length; i++) {
             System.out.println("Parameter: " + args[i]);
         }
-
         if (Objects.equals(args[0], "start")) {
             System.out.println("Start");
-            //new Receiver().run(2000);
-            for (int i = 0; i < Receiver.mac.length; i++) {
-                System.out.print(Receiver.mac[i] + " ");
-            }
+            addr = args[1];
+            new Receiver().run(2000);
         }
+    }
+
+    public static String getAddr() {
+        return addr;
+    }
+
+    public static void setAddr(String addr) {
+        Main.addr = addr;
     }
 }
--- src/com/nuvoled/Receiver.java
+++ src/com/nuvoled/Receiver.java
@@ -3,42 +3,71 @@ package com.nuvoled;
 import java.io.IOException;
 import java.net.*;
 
-public class Receiver extends Thread {
+public class Receiver {
 
-
-    public static void main(String[] args) {
-        int port = args.length == 0 ? 2000 : Integer.parseInt(args[0]);
-        //int port = 2000;
-        new Receiver().run(port);
-    }
-
-
-
-    public static byte[] mac;
+    private static byte[] mac;
+    private static boolean runing;
 
     public void run(int port) {
         try {
-
             DatagramSocket serverSocket = new DatagramSocket(port);
             byte[] receiveData = new byte[15];
-            mac = new byte[2];
+            mac = new byte[4];
 
-            System.out.printf("Listening on udp:%s:%d%n", InetAddress.getLocalHost().getHostAddress(), port);
+            System.out.printf("Listening on udp: %s:%d%n", InetAddress.getLocalHost().getHostAddress(), port);
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            runing = true;
 
-            while (true) {
-                serverSocket.receive(receivePacket);
-                String sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
-                System.out.println("RECEIVED: " + sentence);
+            while (runing) {
+                System.out.println("Searching...");
+                try {
+                    serverSocket.receive(receivePacket);
+                }catch (Exception ex){
+                    System.out.println(ex);
+                }
+                //String sentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                //System.out.println("RECEIVED: " + sentence);
+                System.out.print("RECEIVED: ");
 
                 for (int i = 0; i < receiveData.length; i++) {
                     System.out.print(receiveData[i] + " ");
                 }
-
-                mac[0] = receiveData[3];
-                mac[1] = receiveData[4];
-                mac[2] = receiveData[5];
                 System.out.println("ende");
+
+                if (receiveData[2] == 15) {
+                    runing = false;
+                    mac[0] = receiveData[3];
+                    mac[1] = receiveData[4];
+                    mac[2] = receiveData[5];
+                    mac[3] = receiveData[6];
+                    System.out.println("Pannel Gefunden | Mac: " + mac[0] + mac[1] + mac[2] + mac[3]);
+
+                    byte[] message = new byte[16];
+                    message[0] = 36;
+                    message[1] = 36;
+                    message[2] = (byte) 120;
+                    message[3] = 2;
+                    message[4] = 32;
+                    message[5] = 8;
+                    message[6] = 8;
+                    message[7] = 1;
+                    message[8] = mac[2];
+                    message[9] = mac[3];
+                    message[10] = 74;
+                    message[11] = 8;
+                    message[12] = 8;
+                    message[13] = 0;
+                    message[14] = 0;
+                    message[15] = 0;
+
+                    InetAddress address = InetAddress.getByName(Main.getAddr());
+
+                    //DatagramSocket dsocket = new DatagramSocket();
+                    DatagramPacket senderPacket = new DatagramPacket(message, message.length, address, port);
+                    serverSocket.send(senderPacket);
+                    serverSocket.close();
+
+                }
             }
         } catch (IOException e) {
             System.out.println(e);
