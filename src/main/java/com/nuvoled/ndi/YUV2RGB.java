e0e4ef193e3b3229b366f0af11a6ada981f860ab
Minus345 <106837932+Minus345@users.noreply.github.com>
2025-01-11 01:12:36 +0100

    fixing YUV to RGB Algorithm

--- src/main/java/com/nuvoled/ndi/YUV2RGB.java
+++ src/main/java/com/nuvoled/ndi/YUV2RGB.java
@@ -1,32 +1,43 @@
 package com.nuvoled.ndi;
 
-import java.awt.*;
+import com.sun.xml.bind.v2.TODO;
 
 public class YUV2RGB {
     public static int[] toRGB(int Y0, int U0, int V0) {
 
-        int Y1 = Y0 & 0xff;
-        int U1 = U0 & 0xff;
-        int V1 = V0 & 0xff;
+        double Y1 = (Y0 & 0xff) - 16D;
+        double U1 = (U0 & 0xff) - 128;
+        double V1 = (V0 & 0xff) - 128;
 
-        double R;
-        double G;
-        double B;
+        // Algorithmus von https://de.wikipedia.org/wiki/YUV-Farbmodell
+        double R = Y1 + 1 / 0.877 * V1;
+        double G = Y1 - 0.144 / (0.587 * 0.493) * U1 - 0.299 / (0.587 * 0.877) * V1;
+        double B = Y1 + 1 / 0.493 * U1;
 
-        R = (double) Y1 + (1.4075 * ((double) V1 - 128));
-        G = (double) Y1 - 0.3455 * ((double) U1 - 128) - (0.7169 * ((double) V1 - 128));
-        B = (double) Y1 + 1.7790 * ((double) U1 - 128);
+        /*
+        //Algorithmus von Copilot
+        double R = Y1 + 1.13983 * V1 ;
+        double G = Y1 - 0.39465 * U1 - 0.58060 * V1;
+        double B = Y1 + 2.03211 * U1;
+         */
 
-        int roundR = roundTo255((int)R);
-        int roundG = roundTo255((int)G);
-        int roundB = roundTo255((int)B);
+        int roundR = roundTo255((int) R);
+        int roundG = roundTo255((int) G);
+        int roundB = roundTo255((int) B);
 
-        //System.out.println(roundR + "," + roundG + "," + roundB);
+        // schwarz korrektur (wegen dem Compression Algorithmus von NDI)
+        //TODO: Make it toogable
+        final int minimalValue = 10;
+
+        if (roundR < minimalValue) roundR = 0;
+        if (roundG < minimalValue) roundG = 0;
+        if (roundB < minimalValue) roundB = 0;
 
         int[] outputRGB = new int[3];
         outputRGB[0] = roundR;
         outputRGB[1] = roundG;
         outputRGB[2] = roundB;
+
         return outputRGB;
     }
 
